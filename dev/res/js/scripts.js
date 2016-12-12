5577769cc490d85dee9e3405ff8d3f36b1aa5f16
Ilya Biryukov <uncle.funkay@gmail.com>
2016-12-12 16:07:11 +0300

    Build

--- dev/res/js/scripts.js
+++ dev/res/js/scripts.js
@@ -1,4 +1,5 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+
 /* jshint browser:true */
 
 'use strict';
@@ -26,6 +27,7 @@ function runAnimation(startTime, duration, startValue, deltaValue, drawingContex
 exports.run = runAnimation;
 
 },{}],2:[function(require,module,exports){
+
 /* jshint browser:true */
 
 'use strict';
@@ -138,6 +140,7 @@ module.exports = function (elementIDs, imageURLs, positionShift) {
 };
 
 },{"animation":1}],3:[function(require,module,exports){
+
 /* jshint browser:true */
 
 'use strict';
